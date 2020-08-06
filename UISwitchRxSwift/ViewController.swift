3ddee70f8c49de75035830bfcee94e98c0f1a5d1
Сычев Евгений Дмитриевич <ydsychev@www.mts.ru>
2020-08-06 18:34:01 +0300

    default application

---
+++ UISwitchRxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UISwitchRxSwift
+//
+//  Created by Сычев Евгений Дмитриевич on 06.08.2020.
+//  Copyright © 2020 Сычев Евгений Дмитриевич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
